496b77998d6f5c347cefb0f59d47eaaf536f223a
popalay <popalay1@gmail.com>
2020-07-16 12:44:17 +0800

    Initial Commit

---
+++ Shared/TracktorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TracktorApp.swift
+//  Shared
+//
+//  Created by Денис Никифоров on 16.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct TracktorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
